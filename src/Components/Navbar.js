5bb65f0387830da7e3bee30f26a5310bc8b24778
aslam48 <youngfizzy001@gmail.com>
2022-09-15 16:42:35 +0100

    Contex-and-Hook

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class Navbar  extends Component {
+    render() { 
+        return (
+            <nav> 
+                <h1>content</h1>
+                <ul>
+                    <li>home</li>
+                    <li>about</li>
+                    <li>contact</li>
+                </ul>
+         </nav>
+        );
+    }
+}
+ 
+export default Navbar
