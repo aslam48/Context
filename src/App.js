d5d0428f1bfd840d0b96b5a111ce0e82f3e41cda
aslam48 <youngfizzy001@gmail.com>
2022-09-16 16:59:03 +0100

    context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import BookList from './Components/BookList';
+import ThemeContextProvider from './Components/Contents/ThemeContext';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
+      <ThemeContextProvider>
+      <Navbar/>
      <BookList/>
+      </ThemeContextProvider>
+   
     </div>
   );
 }
